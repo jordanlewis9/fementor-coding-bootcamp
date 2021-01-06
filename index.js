9194649ddd2700a8a8a8e2def3d29218eb24439d
Jordan <jlewis2008@live.com>
2021-01-06 09:31:21 -0500

    Bug fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const quote1 = document.querySelector('.testimonial__quote--1');
 const quote2 = document.querySelector('.testimonial__quote--2');
 const author1 = document.querySelector('.testimonial__quote--1-author');
 const author2 = document.querySelector('.testimonial__quote--2-author');
+const picContainer = document.querySelector('.testimonial__picture--container');
 const pic1 = document.querySelector('.testimonial__picture--1');
 const pic2 = document.querySelector('.testimonial__picture--2');
 const nextSlider = document.querySelector('.slider__next');
@@ -26,11 +27,11 @@ const handleNextTransition = () => {
   array2.forEach(item => item.classList.remove('hide'));
   pic2.classList.add('picture-slide-in-right');
   document.removeEventListener('keydown', handleKeyup);
+  nextSlider.removeEventListener('click', handleNextTransition);
   nextSlider.classList.remove('pointer');
   setTimeout(() => {
     prevSlider.addEventListener('click', handlePrevTransition);
     prevSlider.classList.add('pointer');
-    nextSlider.removeEventListener('click', handleNextTransition);
     pic2.classList.remove('picture-slide-in-right');
     document.addEventListener('keydown', handleKeyup);
   }, 500);
@@ -42,11 +43,11 @@ const handlePrevTransition = () => {
   array2.forEach(item => item.classList.add('hide'));
   pic1.classList.add('picture-slide-in-left');
   document.removeEventListener('keydown', handleKeyup);
+  prevSlider.removeEventListener('click', handlePrevTransition);
   prevSlider.classList.remove('pointer');
   setTimeout(() => {
     nextSlider.addEventListener('click', handleNextTransition);
     nextSlider.classList.add('pointer');
-    prevSlider.removeEventListener('click', handlePrevTransition);
     pic1.classList.remove('picture-slide-in-left');
     document.addEventListener('keydown', handleKeyup);
     }, 500);
